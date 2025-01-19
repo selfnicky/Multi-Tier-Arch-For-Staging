2d591d484db6a48a107a4e9145d016dc43421b1d
Nicky <selfnicky@gmail.com>
2025-01-19 03:50:42 +0100

    Multi-Tier-Arch-For-Staging

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "selfterraform" # your bucket name
+    bucket         = "" # your bucket name
     key            = "demo/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-statefile-lock" # your dynamoDB table name
+    dynamodb_table = "" # your dynamoDB table name
   }
 }
